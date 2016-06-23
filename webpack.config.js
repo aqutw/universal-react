377dab9157043e83a3ce33595ff146dc59fd1cd3
aqutw <aquarian.ex@gmail.com>
2016-06-23 23:51:39 +0800

    setup/configure css scss extract-text-webpack-plugin at webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   devtool: '#source-map',
@@ -17,10 +18,16 @@ module.exports = {
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin('[name].css'),
     new webpack.NoErrorsPlugin()
   ],
   module: {
-    loaders: [{
+    loaders: [
+      { test: /\.scss$/, loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap' 
+      },{
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', "css-loader")
+      },{
       test: /\.js?$/,
       loader: 'babel',
       include: path.join(__dirname, 'app'),
